79fe82cdab9b3aced6d32ea2d4afc8dbeea8bf95
wangxingxing <xingxing_wang@dse.cn>
2021-07-07 18:27:05 +0800

    新增登录页面

---
+++ frontend/src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-input v-model="user" placeholder="请输入账户"></el-input>
+    <el-input v-model="password" type="password" placeholder="请输入密码"></el-input>
+    <el-button type="primary">登录</el-button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      user: '',
+      password: '',
+    }
+  }
+}
+</script>
